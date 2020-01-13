e96622d4fb4a3ac3c4eea533130d29890d1929d8
koperniki <ypsilonik@mail.ru>
2020-01-14 00:09:17 +0700

    initial comit

---
+++ Data/Block.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Blockchain.Data
+{
+    public class MainingBlock
+    {
+
+        public EventHandler<string> HashChanged { get; set; }
+        private int _blockNum;
+        private int _nonce;
+        private string _prevHash;
+        private string _data;
+        private readonly MainingBlock _prevBlock;
+
+        public MainingBlock(MainingBlock prevBlock)
+        {
+            _prevBlock = prevBlock;
+            if (_prevBlock != null)
+            {
+                _prevBlock.HashChanged += (sender, s) => PrevHash = s;
+                PrevHash = _prevBlock.Hash;
+            }
+        }
+
+        public int BlockNum
+        {
+            get => _blockNum;
+            set
+            {
+                _blockNum = value;
+                GetHash();
+            }
+        }
+
+
+
+        public int Nonce
+        {
+            get => _nonce;
+            set
+            {
+                _nonce = value;
+                GetHash();
+            }
+        }
+
+        public string PrevHash
+        {
+            get => _prevHash;
+            set
+            {
+                _prevHash = value;
+                GetHash();
+            }
+        }
+
+        public string Data
+        {
+            get => _data;
+            set
+            {
+                _data = value;
+                GetHash();
+            }
+        }
+
+
+        public string Hash { get; set; }
+
+        public void GetHash()
+        {
+            Hash = ShaService.GetSha(_blockNum + _data + _prevHash + _nonce);
+            HashChanged?.Invoke(this, Hash);
+        }
+
+        public bool IsGood()
+        {
+            if (string.IsNullOrWhiteSpace(Hash))
+                return false;
+
+            return Hash.StartsWith("00000");
+        }
+
+        public void Mine()
+        {
+            Nonce = 0;
+            while (!IsGood())
+            {
+                Nonce++;
+            }
+        }
+    }
+}
---
+++ Data/ShaService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Blockchain.Data
+{
+    public class ShaService
+    {
+        private static SHA256 _sha = SHA256.Create();
+
+        public static string GetSha(string data)
+        {
+            var bytes = Encoding.UTF8.GetBytes(data);
+            var shaBytes = _sha.ComputeHash(bytes);
+            return BitConverter.ToString(shaBytes).Replace("-", string.Empty).ToLower();
+        }
+    }
+}
